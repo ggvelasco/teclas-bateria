d1a7f84befaa2e4290dbe25e2dbfa7356482ef13
gvelas1337 <gvelasco.dev@gmail.com>
2022-07-24 03:53:45 -0300

    primeiro commit - adicionando o projeto ao github

---
+++ main.js
@@ -0,0 +1,8 @@
+// POM -----------------------------------------------------
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelectorAll('.tecla')
+
+
